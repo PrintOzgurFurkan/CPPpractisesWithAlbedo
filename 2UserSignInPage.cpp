40229c27f1c40512a52b51c80714dc95caa912bb
Furkan (Ozgur) Yavuz <128408299+printalbedoafel@users.noreply.github.com>
2023-07-19 09:01:12 +0300

    Basic Input Output Practises
    I made a basic code seems like sign in page

---
+++ 2UserSignInPage.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+	// oyuncudan bilgi alinan nokta
+
+
+	string username;
+	string usersurname;
+	string usernick;
+	int phonenumber;
+	string email;
+	string  birthday;
+	string  birthday2;
+	string  birthday3;
+	int passowrd;
+	
+
+	cout << endl << endl << endl << "							WELCOME			" << endl << endl << endl ;
+	cout << "							SIGN IN			" << endl << endl << endl;
+	cout << "Enter Your Name : ";
+	cin >> username; 
+	cout << "Enter Your surname : ";
+	cin >> usersurname  ;
+	cout << endl << endl << "Enter Your User Name : " << endl;
+	cin >> usernick;
+	cout << endl << endl << "Enter Your Birthday (DD/MM/YY)" << endl;
+	cin >> birthday >> birthday2  >> birthday3 ;
+	cout << endl << endl << "Enter Your number : " << endl;
+	cin >> phonenumber;
+	cout << endl << endl << "Enter Your E-mail Adress : " << endl;
+	cin >> email;
+	cout << endl << endl << "Enter Password : " << endl;
+	cin >> passowrd;
+
+	cout << endl << endl << endl << "							CONFIRM		" << endl << endl << endl << endl << endl << endl;
+
+
+
+
+		return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
